aa8e446791f904801edc2f153c5a6b078ccb1a4d
Kevin Coakley <kcoakley@sdsc.edu>
2018-08-03 13:01:32 -0700

    Example R script to use downloadtweets

---
+++ example/example.R
@@ -0,0 +1,36 @@
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+
+# install downloadtweets from github
+devtools::install_github("kevincoakley/downloadtweets")
+
+library(downloadtweets)
+
+saved_tweets <- "tweets.Rda"
+tweet_ids_file <- "ids.txt" # Example from https://archive.org/details/gaza-tweets
+
+# Set the working directory
+setwd('/Users/kcoakley/temp/R/')
+
+# If saved_tweets exist, load the tweets from disk, otherwise download the tweets from Twitter
+if(file.exists(saved_tweets)){
+  load(saved_tweets)
+} else {
+  # Get Twitter api keys from https://apps.twitter.com
+  consumerKey <- ''
+  consumerSecret <- ''
+  accessToken <- ''
+  accessTokenSecret <- ''
+
+  # Read tweet ids
+  tweet_ids <- data.frame(read.table(tweet_ids_file, numerals = 'no.loss'))
+
+  # Download tweets
+  tweets <- download_tweets(consumerKey, consumerSecret, accessToken, accessTokenSecret, tweet_ids)
+
+  # Save tweets to disk
+  save(tweets, file=saved_tweets)
+}
+
+show(tweets)
